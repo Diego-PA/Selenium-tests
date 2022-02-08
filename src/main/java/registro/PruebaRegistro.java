f43ad948df5e3dadbf87db1982072829c313ad0a
DiegoPA <diego.loge@hotmail.com>
2022-02-08 12:34:51 -0600

    Añadiendo ChromeDriver.exe

--- src/main/java/registro/PruebaRegistro.java
+++ src/main/java/registro/PruebaRegistro.java
@@ -2,24 +2,41 @@ package registro;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import static java.lang.System.*;
 
 public class PruebaRegistro {
     public static void main(String[] args) {
 
+        //Importante: Para poder interactuar con los elementos deben de estar visibles.
+
+        /*
+        //Para ms edge
         //Elegimos el driver
         setProperty("webdriver.edge.driver", "C:\\Users\\diego\\IdeaProjects\\SeleniumTestProject\\src\\Drivers\\msedgedriver.exe");
 
         //Inicializa el navegador
         WebDriver driver = new EdgeDriver();
+        */
+
+        //Para chrome
+        //Elegimos el driver
+        setProperty("webdriver.chrome.driver", "C:\\Users\\diego\\IdeaProjects\\SeleniumTestProject\\src\\Drivers\\chromedriver.exe");
+
+        //Inicializa el navegador
+        WebDriver driver = new ChromeDriver();
+
+        //Le damos la propiedad de ejecutar scripts de JS
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        driver.manage().window().maximize();
 
         //Abrimos la página web
         driver.get("http://127.0.0.1:8001/control/register");
-
-        driver.manage().window().maximize();
 
         //Llenamos el formulario
         driver.findElement(By.id("name")).sendKeys("Juan");
@@ -31,29 +48,18 @@ public class PruebaRegistro {
         selPlantel.selectByValue("411");
 
         //Checkbox Carrera
-        /* WebElement checkCarrera = driver.findElement(By.id("carrera_110"));
-        checkCarrera.click(); */
         driver.findElement(By.id("carrera_110")).click();
-        driver.findElement(By.id("permiso_9")).click();
 
         //Credenciales
         driver.findElement(By.id("email")).sendKeys("correo@mail.com");
         driver.findElement(By.id("password")).sendKeys("hola123.");
         driver.findElement(By.id("password_confirmation")).sendKeys("hola123.");
 
+        //Scroll al fondo
+        js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+
         //Permisos
-        /*
-        System.out.println("Hola");
-        try{
-            driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/form/div[9]/input[1]")).click();
-        }catch(Exception e){
-            out.println(e);
-        }
-        WebElement checkP9 = driver.findElement(By.id("permiso_10"));
-        checkP9.click();
-
-         */
-
+        driver.findElement(By.id("permiso_9")).click();
         driver.findElement(By.id("permiso_10")).click();
         driver.findElement(By.id("permiso_11")).click();
         driver.findElement(By.id("permiso_12")).click();
@@ -63,5 +69,8 @@ public class PruebaRegistro {
         driver.findElement(By.id("permiso_16")).click();
         driver.findElement(By.id("permiso_17")).click();
         driver.findElement(By.id("permiso_18")).click();
+
+        //enviar
+        driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/form/div[10]/button")).click();
     }
 }
